431ec4e24f0a337382cf8738e7556557cda21549
boo105 <boo10500@naver.com>
2024-03-27 23:32:07 +0900

    Rust 공부를 위한 Tokio 구축 init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use tokio::time;
+
+async fn run() {
+    log::info!("Sleeping!");
+    time::sleep(time::Duration::from_secs(1)).await;
+    log::info!("Awake!");
+}
+
+fn main() {
+    simple_logger::init_wi
+    println!("Hello, world!");
+}
